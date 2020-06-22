a7420731016b922146f89ab124f340dd824277ea
李浩 <971410648@qq.com>
2020-06-22 13:26:45 +0800

    对体检预约.txt  做了修改  完善了里面的一些话术

---
+++ health_parent/java_offer/src/main/java/cn/lihao/test/DebugList.java
@@ -0,0 +1,21 @@
+package cn.lihao.test;
+
+public class DebugList {
+
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 50; i++) {
+            System.out.println(i);
+        }
+
+
+        System.out.println("1212");
+
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println("5555555555555");
+        }
+    }
+
+}
